7fd65d53943710db36b2acf111f0c240d9ac8daf
Gloria Lim <glorialimartt@gmail.com>
2024-02-27 22:21:31 -0500

    Implement benchmarking code

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,11 @@
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+  result_from_block = yield
+  end_time = Time.now
+
+  return end_time - start_time
+
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
